27ba2b5f65615bfb7187aaf19737e872f8e2a11e
Venkat-aero <102429012+Venkat-aero@users.noreply.github.com>
2022-06-12 17:51:13 +0530

    Create script.js

---
+++ length/script.js
@@ -0,0 +1,18 @@
+
+var arr1 = [,,,]; 
+var arr2 = new Array(3);
+var arr3 = [1,2,3,4,5];
+var array = [ [1,2,3], [4,5,6]  ];
+var arrayLength = [ [1,2,3], [4,5,6]  ];
+
+console.log(  arr1 , arr1.length  );
+console.log( arr2 , arr2.length);
+console.log(arr3 , arr3.length);
+console.log( array , array.length);
+console.log("-------------------");
+console.log("var array[0].length = [ [1,2,3], [4,5,6]  ];");
+console.log("i did not understand this variable but i tried ");
+for (i = 0 ; i < arrayLength.length; i++)
+{
+    console.log(arrayLength[0] , arrayLength.length);
+}
