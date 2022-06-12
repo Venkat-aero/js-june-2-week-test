ae1f6962cbc8f9e4bb61a13263d206ef179c499b
Venkat-aero <102429012+Venkat-aero@users.noreply.github.com>
2022-06-12 18:00:43 +0530

    Create script.js

---
+++ index/script.js
@@ -0,0 +1,9 @@
+
+// finding index position  of b 
+
+var arr= ['a','b','c','d'];
+for (i = 0 ; i < arr.length; i ++)
+{
+    if (arr[i] === 'b')
+    console.log("The index position of b is ", i);
+}
