5b5cc760ce5fa606dd5118184b3e3462b6c9c286
Venkat-aero <102429012+Venkat-aero@users.noreply.github.com>
2022-06-12 19:21:14 +0530

    Create script.js

---
+++ largest/script.js
@@ -0,0 +1,10 @@
+// finding largest number in an array
+
+let a = [ 2, 5 ,9 , 12, 24, 45, 67, 89, 97, 106];
+console.log(a , a.length);
+for (i=0; i < a.length; i++){
+ 
+        console.log( a[a.length - 1], " is the largest element");
+     
+}
+console.log(a.length -1 , " is the index of the number " , a[a.length - 1] );
